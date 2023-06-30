9beefbe057e27c01ddeabd55f8db83579b90a565
Saverio Napolitano <saverio.napolitano.2002@gmail.com>
2023-07-01 00:41:23 +0200

    Enhanced double_last

--- exercises/python/tuple/double_last.py
+++ exercises/python/tuple/double_last.py
@@ -1,7 +1,3 @@
-def double_last(items): #TODO enhance
-    for ind, item in enumerate(items):
-        new_item = []
-        for index, element in enumerate(item):
-            new_item.append(element if index != len(item) - 1 else element * 2)
-        items[ind] = tuple(new_item)
-    return items
+def double_last(items):
+    return [tuple([(element if index != len(item) - 1 else element * 2) for index, element in enumerate(item)])
+            for item in items]
